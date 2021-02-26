97a1a5698ded81f06de72318ab52be85f7c318e7
Mick Watkins <mick_watkins94@hotmail.com>
2021-02-26 11:33:35 +1000

    Continued work on day 19 Python part 1

--- day-19/python/day_19.py
+++ day-19/python/day_19.py
@@ -1,21 +1,40 @@
+rule_dict = {}
+
 def create_rule_dict(line_array):
     rule_dict = {}
     for line in line_array:
         split_line = line.split(": ")
+        rule_no = int(split_line[0])
         if '"' in split_line[1]:
-            rule_dict[split_line[0]] = split_line[1].replace('"', "")
+            rule_dict[rule_no] = split_line[1].replace('"', "")
         else:
             dep_rule = split_line[1].split(" | ")
             dep_rule_array = [[int(rule_no) for rule_no in rule.split(" ")] for rule in dep_rule]
-            rule_dict[split_line[0]] = dep_rule_array
+            rule_dict[rule_no] = dep_rule_array
 
     return rule_dict
+
+def check_password(password, rule):
+    return False    # TODO
 
 if __name__=="__main__":
     input_filename = "../test_input.txt"
     f = open(input_filename, 'r')
-    line_array = [line.strip() for line in f.readlines()]
 
-    rule_dict = create_rule_dict(line_array)
+    rule_array = []
+    password_array = []
+    file_rules = True
+
+    for line in f.readlines():
+        line = line.strip()
+        if line == "":
+            file_rules = False
+        elif file_rules:
+            rule_array.append(line)
+        else:
+            password_array.append(line)
+
+    rule_dict = create_rule_dict(rule_array)
 
     print(rule_dict)
+    print(password_array)
