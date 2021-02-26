28998a68aea64fc976ac914688266aed10b20ba7
watksimo <mick_watkins94@hotmail.com>
2021-02-26 23:22:23 +1000

    Continued on day 19

--- day-19/python/day_19.py
+++ day-19/python/day_19.py
@@ -1,3 +1,5 @@
+from iteration_utilities import deepflatten
+
 rule_dict = {}
 
 def create_rule_dict(line_array):
@@ -14,7 +16,51 @@ def create_rule_dict(line_array):
 
     return rule_dict
 
-def check_password(password, rule):
+def recursive_rule_replace(rule):
+    # print(rule)
+    if type(rule) == str:
+        return rule
+
+    new_rule = rule
+    for idx, elem in enumerate(rule):
+        if not type(elem) == str:
+            for idx2, elem2 in enumerate(elem):
+                if type(elem2) == int:
+                    # print(f"Elem2: {elem2}")
+                    new_rule[idx][idx2] = rule_dict[elem2]
+                    return recursive_rule_replace(new_rule)
+                if type(elem2) == list:
+                    flattened_list = list(deepflatten(elem2))
+                    all_chars = True
+                    for flat_elem in flattened_list:
+                        if type(flat_elem) != str:
+                            all_chars = False
+                            break
+                    if all_chars: continue
+                    new_rule[idx][idx2] = recursive_rule_replace(elem2)
+                    return recursive_rule_replace(new_rule)
+
+    return rule
+
+def combine_rule_ands(rule_array):
+    
+    new_array = rule_array
+    type_array = [type(_) for _ in rule_array]
+    if list not in type_array:
+        return "".join(rule_array)
+
+    for idx, test_array in enumerate(rule_array):
+        new_array[idx] = combine_rule_ands(test_array)
+
+    return new_array
+
+
+def check_password(password, rule_key):
+    checked_index = 0
+
+    # curr_rule_key = rule_key
+    # if rule_dict[curr_rule_key]
+
     return False    # TODO
 
 if __name__=="__main__":
@@ -24,6 +70,8 @@ if __name__=="__main__":
     rule_array = []
     password_array = []
     file_rules = True
+
+    # print(password_from_rule([['a','b'], ['c','d']]))
 
     for line in f.readlines():
         line = line.strip()
@@ -36,5 +84,12 @@ if __name__=="__main__":
 
     rule_dict = create_rule_dict(rule_array)
 
-    print(rule_dict)
-    print(password_array)
+    recursive_rule = recursive_rule_replace(rule_dict[7])
+    print(combine_rule_ands(recursive_rule))
+
+    recursive_rule = recursive_rule_replace(rule_dict[6])
+    print(combine_rule_ands(recursive_rule))
+
+    recursive_rule = recursive_rule_replace(rule_dict[0])
+    print(combine_rule_ands(recursive_rule))
+
