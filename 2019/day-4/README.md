a5c8d2f0db4d6cfa43e0efe1ba12ace0013328d7
Mick Watkins <mick_watkins94@hotmail.com>
2021-07-02 12:28:16 +1000

    Completed 2019 day 4 part 2

--- 2019/day-4/README.md
+++ 2019/day-4/README.md
@@ -1,3 +1,4 @@
 
-![Completion](https://img.shields.io/badge/Completed-Part%201-yellow.svg)
+![Completion](https://img.shields.io/badge/Completed-Parts%201%20%26%202-green.svg)
 <h1>Day 4: Secure Container</h1>
+* My priority with this solution was minimising unnecessary checks, the most important component of this was not checking numbers that had decreasing digits.
--- 2019/day-4/python/day4.py
+++ 2019/day-4/python/day4.py
@@ -2,17 +2,9 @@ def load_input(input_filename):
   f = open(input_filename, "r")
   return [int(_) for _ in f.readline().strip().split("-")]
 
-def merge_dicts(dict1, dict2):
-  intersects = []
-  dict3 = {**dict1, **dict2}
-  for key, value in dict3.items():
-    if key in dict1 and key in dict2:
-      dict3[key] = [value , dict1[key]]
-      intersects.append(key)
-  return dict3, intersects
-
 def solve_part1(min_val, max_val):
   valid_passwords = []
+  valid_passwords_2 = []
 
   # Ensure min < max
   if min_val >= max_val: return valid_passwords
@@ -28,29 +20,39 @@ def solve_part1(min_val, max_val):
 
     # Next valid non-decreasing
     adj_digits = False
+    adj_digits_2 = False
+    adj_digits_cnt = 1 # A number by itelf is 1 adj digit (no 0)
     for idx, ch in enumerate(char_array):
       if idx > 0:
         if int(ch) < int(prev_char): char_array[idx] = prev_char
         ch = char_array[idx]
-        if int(ch) == int(prev_char): adj_digits = True
+        if int(ch) == int(prev_char):
+          adj_digits = True
+          adj_digits_cnt += 1
+        else:
+          if adj_digits_cnt == 2: adj_digits_2 = True
+          adj_digits_cnt = 1
       prev_char = ch
+    if adj_digits_cnt == 2: adj_digits_2 = True
     new_pass_int = int(''.join(char_array))
     curr_pass_int = new_pass_int
 
     ## Check password is in range and has adjacent digits
-    if min_val <= curr_pass_int <= max_val and adj_digits:
-      valid_passwords.append(curr_pass_int)
+    if min_val <= curr_pass_int <= max_val:
+      if adj_digits: valid_passwords.append(curr_pass_int)
+      if adj_digits_2: valid_passwords_2.append(curr_pass_int)
     curr_pass_int += 1
 
-  return valid_passwords
+  return valid_passwords, valid_passwords_2
 
 if __name__ == '__main__':
   min_val, max_val = load_input("../input.txt")
 
+  valid_passwords, valid_passwords_2 = solve_part1(min_val, max_val)
+
   # part 1 solution
-  valid_passwords = solve_part1(min_val, max_val)
   print("Part 1 solution: {}".format(len(valid_passwords)))
 
   # part 2 solution
-  # print("Part 2 solution: {}".format(solution))
+  print("Part 2 solution: {}".format(len(valid_passwords_2)))
 
