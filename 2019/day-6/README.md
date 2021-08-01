ed042600921bb3c85a771309987d9a8c2a9280b1
watksimo <mick_watkins94@hotmail.com>
2021-08-01 20:28:00 +1000

    Completed 2019 day 6 part 2

--- 2019/day-6/README.md
+++ 2019/day-6/README.md
@@ -1,3 +1,3 @@
-![Completion](https://img.shields.io/badge/Completed-Part%201-yellow.svg)
+![Completion](https://img.shields.io/badge/Completed-Parts%201%20%26%202-green.svg)
 <h1>Day 6: Universal Orbit Map</h1>
 * Being lazy, used `networkx` library to create a directed-acyclic graph and do all of the work
--- 2019/day-6/python/day6.py
+++ 2019/day-6/python/day6.py
@@ -3,7 +3,7 @@ import networkx as nx
 def load_input(input_filename):
   f = open(input_filename, "r")
   conn_array = [_.strip().split(")") for _ in f.readlines()]
-  graph = nx.DiGraph()
+  graph = nx.Graph()
   graph.add_edges_from(conn_array)
   return graph
 
@@ -13,9 +13,17 @@ def solve_part1(input_graph):
   for node in shortest_path_lengths:
     if node[0] == "COM": return sum(node[1].values())
 
+def solve_part2(input_graph):
+  you_orbit = list(input_graph.edges("YOU"))[0][1]
+  san_orbit = list(input_graph.edges("SAN"))[0][1]
+  return len(nx.shortest_path(input_graph, source=you_orbit, target=san_orbit)) - 1
+
 if __name__ == '__main__':
   input_graph = load_input("../input.txt")
   total_orbits = solve_part1(input_graph)
+  you_to_santa = solve_part2(input_graph)
 
   # Solutions
   print("Part 1: {}".format(total_orbits))
+  print("Part 2: {}".format(you_to_santa))
+  
