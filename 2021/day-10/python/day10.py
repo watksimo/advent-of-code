285b82dbcc919f1349042dbeda35bfaf1a80d1ad
watksimo <mick_watkins94@hotmail.com>
2021-12-28 12:40:40 +1000

    Completed day 10

--- 2021/day-10/python/day10.py
+++ 2021/day-10/python/day10.py
@@ -1,12 +1,93 @@
+from collections import deque
+import math
+
 def load_input(input_filename):
   f = open(input_filename, "r")
-  return [[int(ch1) for ch1 in list(line.strip())] for line in f.readlines()]
+  return [list(line.strip()) for line in f.readlines()]
 
 def solve_part1(input_array):
-  return None
+  close_dict = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '>': '<'
+  }
+
+  score_dict = {
+    ')' : 3,
+    ']' : 57,
+    '}' : 1197,
+    '>' : 25137
+  }
+
+  brace_q = []
+  incorrect_closes = []
+
+  for line in input_array:
+    for brace in line:
+      if brace in close_dict.values():
+        brace_q.append(brace)
+      else:
+        match = brace_q.pop()
+        expected = close_dict[brace]
+        if match != expected: incorrect_closes.append(brace)
+
+  score = sum([score_dict[close_brace] for close_brace in incorrect_closes])
+  return score
+
 
 def solve_part2(input_array):
-  return None
+  
+  close_dict = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '>': '<'
+  }
+  open_dict = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+    '<': '>'
+  }
+
+  score_dict = {
+    ')' : 1,
+    ']' : 2,
+    '}' : 3,
+    '>' : 4
+  }
+
+  line_completions = []
+
+  for line in input_array:
+    brace_q = []
+    for brace in line:
+      if brace in close_dict.values():
+        brace_q.append(brace)
+      else:
+        match = brace_q[-1]
+        expected = close_dict[brace]
+        if match != expected:
+          brace_q = []
+          break
+        else:
+          brace_q.pop()
+
+    if len(brace_q) != 0:
+      cl = []
+      for b in brace_q: cl.insert(0, open_dict[b])
+      line_completions.append(cl)
+
+    score_list = []
+    for lc in line_completions:
+      score = 0
+      for b in lc: score = score * 5 + score_dict[b]
+      score_list.append(score)
+
+    score_list.sort()
+
+  return score_list[math.floor(len(score_list)/2)]
 
 if __name__ == '__main__':
   input_array = load_input("../input.txt")
